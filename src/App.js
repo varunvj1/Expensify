c97b95fc7f1b3f5f9d2e575b51436c4af1b7fd80
Varun Jain <varunvj181@gmail.com>
2021-10-10 11:08:15 +0530

    Imported Main component in the App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 import Details from './components/Details/Details'
+import Main from './components/Main/Main'
 
 import useStyles from "./styles"
 
@@ -12,8 +13,8 @@ const App = () => {
                 <Grid item xs={12} sm={4} >
                     <Details title="Income" />
                 </Grid>
-                <Grid item xs={12} sm={4} >
-                    MAIN
+                <Grid item xs={12} sm={3} >
+                    <Main />
                 </Grid>
                 <Grid item xs={12} sm={4} >
                     <Details title="Expense" />
