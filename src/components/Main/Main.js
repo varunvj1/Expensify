db92c2999d86137cc1697de4cc1d9a6c3438d63e
Varun Jain <varunvj181@gmail.com>
2021-10-10 11:06:58 +0530

    Imported List component in the Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from "@material-ui/core"
 import Form from './Form/Form'
+import List from "./List/List"
 
 import useStyles from "./styles"
 
@@ -21,6 +22,7 @@ const Main = () => {
             <CardContent className={classes.CardContent}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
+                        <List />
                     </Grid>
                 </Grid>
             </CardContent>
