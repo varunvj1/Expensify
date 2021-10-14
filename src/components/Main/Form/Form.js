cddf781a44c9f602e03054c2ea7ecb11637b9ab9
Varun Jain <varunvj181@gmail.com>
2021-10-14 14:23:43 +0530

    Added useState hook to the form, and stored input values on change

--- src/components/Main/Form/Form.js
+++ src/components/Main/Form/Form.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core'
 
 import useStyles from "./styles"
 
+const initialState = {
+    amount: '',
+    category: '',
+    type: 'Income',
+    date: new Date()
+}
+
 const Form = () => {
+    //For styling
     const classes = useStyles()
+
+    //For form data
+    const [formData, setFormData] = useState(initialState)
+    console.log(formData);
 
     return (
         <Grid container spacing={2}>
@@ -16,7 +28,7 @@ const Form = () => {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Type</InputLabel>
-                    <Select>
+                    <Select value={formData.type} onChange={(e) => setFormData({ ...formData, type: e.target.value })}>
                         <MenuItem value="Income">Income</MenuItem>
                         <MenuItem value="Expense">Expense</MenuItem>
                     </Select>
@@ -25,7 +37,7 @@ const Form = () => {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Category</InputLabel>
-                    <Select>
+                    <Select value={formData.category} onChange={(e) => setFormData({ ...formData, category: e.target.value })}>
                         <MenuItem>Option1</MenuItem>
                         <MenuItem>Option1</MenuItem>
                         <MenuItem>Option1</MenuItem>
@@ -33,10 +45,10 @@ const Form = () => {
                 </FormControl>
             </Grid>
             <Grid item xs={6}>
-                <TextField fullWidth label="Amount" type="number" />
+                <TextField fullWidth label="Amount" type="number" value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} />
             </Grid>
             <Grid item xs={6}>
-                <TextField fullWidth label="Date" type="date" />
+                <TextField fullWidth label="Date" type="date" value={formData.date} onChange={(e) => setFormData({ ...formData, date: e.target.value })} />
             </Grid>
             <Button className={classes.button} variant="outlined" color="primary" fullWidth>Create</Button>
         </Grid>
