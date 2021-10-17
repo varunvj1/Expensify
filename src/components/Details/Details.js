ac01297f006c2df9ebfda2e2b72d8a4daf6690d9
Varun Jain <varunvj181@gmail.com>
2021-10-17 15:47:00 +0530

    Imported Chart data from custom hook

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core'
 import { Doughnut } from "react-chartjs-2"
+import useTransactions from '../../useTransactions'
+
 import useStyles from "./styles"
 
 
 const Details = ({ title }) => {
+    //Styling
     const classes = useStyles();
+
+    //Chart data
+    const { total, chartData, } = useTransactions(title);
 
     return (
         <Card className={title === "Income" ? classes.income : classes.expense}>
             <CardHeader title={title} />
             <CardContent>
-                <Typography variant="h5">$50</Typography>
-                {/* <Doughnut data="DATA" /> */}
+                <Typography variant="h5">${total}</Typography>
+                <Doughnut data={chartData} />
             </CardContent>
         </Card>
     )
