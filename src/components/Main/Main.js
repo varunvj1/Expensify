7b76b1fab65e87f15a2d4e234236d9ad38df2481
Varun Jain <varunvj181@gmail.com>
2021-10-22 21:39:05 +0530

    Used context to retrieve and display the total balance

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from "@material-ui/core"
 import Form from './Form/Form'
 import List from "./List/List"
+import { ExpenseTrackerContext } from '../../context/context'
 
 import useStyles from "./styles"
 
 const Main = () => {
     const classes = useStyles();
 
+    const { balance } = useContext(ExpenseTrackerContext)
+
     return (
         <Card className={classes.root}>
             <CardHeader title="Expense Tracker" subheader="Powered By Speechly" />
             <CardContent>
-                <Typography align="center" variant="h5"> Total Balance: $100 </Typography>
+                <Typography align="center" variant="h5"> Total Balance: {balance < 0 ? `-$${0 - balance}` : `$${balance}`} </Typography>
                 <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
                     Try saying: Add income for $100 in Category Salary for Monday..
                 </Typography>
